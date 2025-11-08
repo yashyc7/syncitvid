24b490af3f196d341d6a454ee93dd55f38df9383
yashyc7 <yashchauhanmitcs@gmail.com>
2025-11-08 23:50:46 +0530

    adding some initial code

--- syncitvid/core/models.py
+++ syncitvid/core/models.py
@@ -1,3 +1,28 @@
+# rooms/models.py
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
 
-# Create your models here.
+User = get_user_model()
+
+class Room(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    host = models.ForeignKey(User, related_name="hosted_rooms", on_delete=models.CASCADE)
+    participant = models.ForeignKey(
+        User, related_name="joined_rooms", on_delete=models.CASCADE, null=True, blank=True
+    )
+    invite_code = models.CharField(max_length=20, unique=True, blank=True)
+    participant_joined_at = models.DateTimeField(blank=True, null=True)
+    last_participant_name = models.CharField(max_length=100, blank=True)
+    last_participant_leaved_at = models.DateTimeField(blank=True, null=True)
+    room_created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.invite_code:
+            import uuid
+            self.invite_code = uuid.uuid4().hex[:8].upper()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name} ({self.invite_code})"
